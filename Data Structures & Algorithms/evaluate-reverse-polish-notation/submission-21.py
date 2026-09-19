9a02916ae3ea57585b0f0f23a4f2eaeb59f63051
Aashish Singla <aashishsingla567@gmail.com>
2026-09-19 11:25:28 +0530

    Add: evaluate-reverse-polish-notation - submission-21

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-21.py
@@ -0,0 +1,29 @@
+def isnum(a: str):
+    return a[-1].isdigit()
+
+class Solution:
+    def op(self, a, b, op):
+        if op == "+":
+            return a + b
+        if op == "-":
+            return a - b
+        if op == "*":
+            return a * b
+        if op == "/":
+            if a * b > 0:
+                return a // b
+            return int(a / b)
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        n = len(tokens)
+        i = 0
+        while i < n:
+            if isnum(tokens[i]):
+                stack.append(int(tokens[i]))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(self.op(a, b, tokens[i]))
+            i += 1
+        return stack.pop()
